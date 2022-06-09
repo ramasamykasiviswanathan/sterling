cf8d3527a324039c676f049559cfcfa430def2d3
Ramasamy Kasiviswanathan <kramasamy1991@gmail.com>
2022-06-08 20:49:33 -0400

    Added default get endpoint

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const host = "0.0.0.0";
 app.listen(port, () => {
   console.log("Server started in the port ", port);
 });
+
+app.get("/", (req, res) => {
+  res.send("Error occurred please refresh the page!");
+});
