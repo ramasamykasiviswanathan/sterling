535d781df7f5a6a979e4f5734d1c05c6ba07d67c
Ramasamy Kasiviswanathan <kramasamy1991@gmail.com>
2022-06-08 22:08:44 -0400

    Added code for the postgres connection

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const geoip = require("geoip-lite");
+const path = require("path");
+const { application_name } = require("pg/lib/defaults");
 const app = express();
 const port = process.env.PORT || 3000;
 const host = "0.0.0.0";
@@ -7,8 +9,14 @@ const host = "0.0.0.0";
 app.listen(port, () => {
   console.log("Server started in the port ", port);
 });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+app.get("/api/:geo", (req, res) => {
+  console.log(req.params.geo);
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   console.log("Request IP address is ", ip);
   console.log("geo location is", geoip.lookup(ip));
